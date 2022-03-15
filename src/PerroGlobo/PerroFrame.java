512c56309414b05f294f1e79759e0b611d45cad0
osvaldovilla572 <osvaldovilla572@gmail.com>
2022-03-15 01:05:14 -0600

    Carga inicial

--- src/PerroGlobo/PerroFrame.java
+++ src/PerroGlobo/PerroFrame.java
@@ -50,11 +50,11 @@ public class PerroFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 553, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 436, Short.MAX_VALUE)
         );
 
         pack();
